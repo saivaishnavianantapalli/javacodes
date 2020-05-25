360bdee1c32be0a8b6eb28f9ab6ec8963a09346d
Sai Vaishnavi <46553466+saivaishnavianantapalli@users.noreply.github.com>
2020-05-25 09:36:04 +0530

    Create AddOneToNumber.java

---
+++ AddOneToNumber.java
@@ -0,0 +1,40 @@
+public class AddOneToNumber{
+    public ArrayList<Integer> plusOne(ArrayList<Integer> A) {
+        int len = A.size();
+        int sum = A.get(len-1)+1;
+        if(sum <= 9){
+            A.set(len-1,sum);
+        }
+        else{
+            A.set(len-1,sum%10);
+            int carry = 1;
+            int i=len-2;
+            for(;i >= 0;i--){
+                sum = A.get(i)+carry;
+                if(sum <= 9){
+                    A.set(i,sum);
+                    break;
+                }
+                else{
+                    A.set(i,sum%10);
+                    carry = 1;
+                }
+            }
+            if(i == -1){
+                A.add(0,1);
+            }
+        }
+        int z=0;
+        for(;z < A.size();){
+            if(A.get(z) == 0){
+                z++;
+            }
+            else
+                break;
+        }
+        for(int p=z-1;p >= 0;p--){
+            A.remove(p);
+        }
+        return A;
+    }
+}
